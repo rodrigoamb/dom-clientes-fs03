cb04f3ead881268c51c4fbeada798ee94f816d36
Rodrigo Medeiros <dev.rodrigomedeiros@gmail.com>
2025-07-03 18:54:39 -0300

    parte 1

---
+++ script.js
@@ -0,0 +1,11 @@
+const form = document.getElementById("form-cliente");
+const tabela = document.getElementById("tabela-clientes");
+const modalEditar = document.getElementById("modal-editar");
+const modalExcluir = document.getElementById("modal-excluir");
+
+//---- inputs ----
+
+const inputNome = document.getElementById("nome");
+const inputSobrenome = document.getElementById("sobrenome");
+const inputCpf = document.getElementById("cpf");
+const inputEmail = document.getElementById("email");
