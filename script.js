f16ed317141a6576eeacfddcca0a00da8a839f26
Rodrigo Medeiros <dev.rodrigomedeiros@gmail.com>
2025-07-03 20:00:06 -0300

    rederizar tabela

--- script.js
+++ script.js
@@ -23,3 +23,57 @@ const formEdicao = document.getElementById("form-edicao");
 const btnCancelarEdicao = document.getElementById("cancelar-edicao");
 const btnConfirmarExclusao = document.getElementById("confirmar-exclusao");
 const btnCancelarExclusao = document.getElementById("cancelar-exclusao");
+
+let clientes = [
+  {
+    nome: "Rodrigo",
+    sobrenome: "Medeiros",
+    cpf: "937.837.847-42",
+    email: "rodrigo@gmail.com",
+  },
+  {
+    nome: "Lucas",
+    sobrenome: "Monteiro",
+    cpf: "937.837.999-42",
+    email: "lucas@gmail.com",
+  },
+  {
+    nome: "Ana",
+    sobrenome: "Maria",
+    cpf: "937.847.847-42",
+    email: "Ana@gmail.com",
+  },
+];
+
+function renderizarTabela() {
+  clientes.forEach((cliente) => {
+    const tr = document.createElement("tr");
+
+    tr.innerHTML = `
+        <td>${cliente.nome}</td>
+        <td>${cliente.sobrenome}</td>
+        <td>${cliente.cpf}</td>
+        <td>${cliente.email}</td>
+    `;
+
+    const tdAcoes = document.createElement("td");
+    tdAcoes.classList.add("acoes");
+
+    const btnEditar = document.createElement("button");
+    btnEditar.textContent = "Editar";
+    btnEditar.classList.add("editar");
+
+    const btnExcluir = document.createElement("button");
+    btnExcluir.textContent = "Excluir";
+    btnExcluir.classList.add("excluir");
+
+    tdAcoes.appendChild(btnEditar);
+    tdAcoes.appendChild(btnExcluir);
+
+    tr.appendChild(tdAcoes);
+
+    tabela.appendChild(tr);
+  });
+}
+
+renderizarTabela();
