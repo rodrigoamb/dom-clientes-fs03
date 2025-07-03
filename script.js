eed48a0559659e802d5f7317e3a0dc9281782fa2
Rodrigo Medeiros <dev.rodrigomedeiros@gmail.com>
2025-07-03 19:05:37 -0300

    capturas finalizadas

--- script.js
+++ script.js
@@ -11,3 +11,15 @@ const inputCpf = document.getElementById("cpf");
 const inputEmail = document.getElementById("email");
 
 // ---- inputs do modal editar ----
+
+const inputEditNome = document.getElementById("edit-nome");
+const inputEditSobrenome = document.getElementById("edit-sobrenome");
+const inputEditCpf = document.getElementById("edit-cpf");
+const inputEditEmail = document.getElementById("edit-email");
+
+// --- capturando form de edicao e botoes
+
+const formEdicao = document.getElementById("form-edicao");
+const btnCancelarEdicao = document.getElementById("cancelar-edicao");
+const btnConfirmarExclusao = document.getElementById("confirmar-exclusao");
+const btnCancelarExclusao = document.getElementById("cancelar-exclusao");
